b98433748ee228597c6373e8ced0fa93196069ef
Bill Walker <falklian@gmail.com>
2019-11-10 10:51:30 -0600

    Add .gitignore, convert widget to JSX

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ big-date
 A 10-Foot-HUD style widget for Übersicht that displays the current date and time
 
 ![Big-Date](https://raw.githubusercontent.com/Falklian/big-date/master/screenshot.png "Big-Date")
+
+Download `big-date.widget.zip` and unzip directly into your Übersicht widgets directory
+
+Source
+------
+
+Clone the repository and run `yarn install` or `npm install` in the `big-date.widget` directory. Relies on the `moment` node module.
---
+++ big-date.widget/index.jsx
@@ -0,0 +1,97 @@
+import moment from 'moment';
+
+// Friday November 14 2014  8:43 AM
+const momentFormat = 'dddd MMMM DD YYYY h:mm A';
+
+//export const command = "date '+%A %B %d %Y %l:%M %p'";
+export const command = (dispatch) => {
+    dispatch({
+        type: 'SUCCEDED',
+        data: moment().format(momentFormat)
+    });
+}
+
+export const init = (dispatch) => {
+    dispatch({
+        type: 'SUCCEDED',
+        data: moment().format(momentFormat)
+    });
+}
+
+export const initialState = {
+    date: {
+        dayOfWeek: 'TUESDAY',
+        month: 'JANUARY',
+        dayOfMonth: '11',
+        year: '1983',
+        time: '12:00',
+        amPm: 'AM'
+    }
+};
+
+export const updateState = (event, previousState) => {
+    console.log(previousState);
+    switch(event.type) {
+        case 'SUCCEDED':
+            const parts = moment().format(momentFormat).split(/\s+/);
+            const date = {
+                dayOfWeek: parts[0].toUpperCase(),
+                month: parts[1].toUpperCase(),
+                dayOfMonth: parts[2],
+                year: parts[3],
+                time: parts[4],
+                amPm: parts[5].toUpperCase()
+            }
+            return {
+                date: date
+            };
+        default: {
+            return previousState;
+        }
+    }
+};
+
+export const refreshFrequency = 1000;
+
+export const render = ({ date }) => {
+    return (
+        <div>
+            <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css' />
+            <div className="left">
+                { date.dayOfWeek }<br />
+                { date.month }<br />
+                <span>{ date.time }</span>
+            </div>
+            <div className="right">
+            <span>{ date.dayOfMonth }</span><br />
+            { date.year }<br />
+            { date.amPm }
+            </div>
+        </div>
+    );
+};
+
+export const className = {
+    top: '10px',
+    left: '10px',
+    lineHeight: '.85em',
+    fontSize: '4em',
+    fontFamily: '\'Open sans\', sans-serif',
+    textShadow: '2px 2px 5px #000',
+    color: '#fff',
+    '.left': {
+        textAlign: 'right',
+        float: 'left'
+    },
+    '.right': {
+        paddingLeft: '.2em',
+        float: 'left'
+    },
+    div: {
+        fontWeight: 300
+    },
+    span: {
+        color: '#a9a9a9',
+        fontWeight: 600
+    }
+};
